3ccd00153b89cd1ab9a872d86d12f8740ae8f049
Xinhang Chen <1417798061@qq.com>
2022-01-14 17:37:24 -0600

    add test case for crime data

--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -3,7 +3,22 @@ package parser
 import (
 	"fmt"
 	"testing"
+	"runtime"
+	nc "../nanocube"
 )
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+return b / 1024 / 1024
+}
+
 
 func TestReadCSV(t *testing.T) {
 	fmt.Println(ReadCsvFile("test.csv"))
@@ -22,5 +37,13 @@ func TestNanoCubeFromSmallFile(t *testing.T) {
 }
 
 func TestNanoCubeFromBigFile(t *testing.T) {
-	fmt.Println(CreateNanoCubeFromCsvFile("crime2019.csv", "Primary Type", 16))
+	n := CreateNanoCubeFromCsvFile("crime2020.csv", "Primary Type", 16)
+	PrintMemUsage()
+	fmt.Println(n.Root.Children[0].CatRoot.Summary)
+	fmt.Println(n.Root.Children[1].CatRoot.Summary)
+	fmt.Println(n.Root.Children[2].CatRoot.Summary)
+	fmt.Println(n.Root.Children[3].CatRoot.Summary)
+	fmt.Println(nc.Query(n.Root, nc.Bounds{-87.65,41.8,0.3,0.3},5))
 }
+
+
